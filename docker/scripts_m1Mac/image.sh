959be0283768c6531ec4ab3ec22db4517135a59f
husa <pxtqwe123>
2022-07-15 11:02:16 +0800

    mac dev script update

---
+++ docker/scripts_m1Mac/image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+
+
+#operation in compile image
+cd /share_file/bitnetwork
+make install
+mv $GOPATH/bin/bitnetworkd /share_file/bitnetworkd
+
---
+++ docker/scripts_m1Mac/rollback-peer5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker stop peer5
+sudo rm -rf /usr/bin/bitnetworkd
+sudo cp -r ~/share_file/bitnetworkd /usr/bin
+sudo bitnetworkd rollback-any --home ~/bitnetwork_dev/validators/validator5/bitnetwork/ --height 20
+
+sleep 20
+docker start peer5
+docker logs -f peer5 --tail=100
